3b652c68794cdf217ace268cd38db2de3a632554
Tommm77 <picout.tom@gmail.com>
2021-05-27 10:09:46 +0200

    first commit

---
+++ portfolio/src/Controller/PortfolioController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortfolioController extends AbstractController
+{
+    /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function index(): Response
+    {
+        return $this->render('portfolio/index.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+
+    public function home()
+    {
+        return $this->render('portfolio/home.html.twig');
+    }
+}
