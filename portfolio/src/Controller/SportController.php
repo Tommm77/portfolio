824abb599c2a0c8c09d60a99fbf787a87e936acf
Tommm77 <picout.tom@gmail.com>
2021-05-31 17:47:19 +0200

    7th commit

---
+++ portfolio/src/Controller/SportController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SportController extends AbstractController
+{
+    /**
+     * @Route("/sport", name="sport")
+     */
+    public function index(): Response
+    {
+        return $this->render('sport/index.html.twig', [
+            'controller_name' => 'SportController',
+        ]);
+    }
+}
